18c472983ca06b8c9abcd8de292fdd2c40c73b0d
jBob <91766736+jamesrmilne@users.noreply.github.com>
2025-02-05 13:44:10 +1000

    Update Hyper-V-Manager-Web.ps1

--- src/Hyper-V-Manager-Web.ps1
+++ src/Hyper-V-Manager-Web.ps1
@@ -37,7 +37,7 @@ Write-host "$appName `n`n" -ForegroundColor White
 # Start Runtime Variables
 #
 $redirectToJobs=$false 		# Set this to true to redirect to /job/list when a export/archive is submitted
-$ClobberZips=$false 		# Set this to $true to overwrite
+$ClobberZips=$false # Set this to $true to overwrite
 $BasePath="C:\temp"
 
 #
@@ -203,7 +203,16 @@ $OptimizeIcon = @"
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m21.224 15.543-.813-1.464-1.748.972.812 1.461c.048.085.082.173.104.264a1.024 1.024 0 0 1-.014.5.988.988 0 0 1-.104.235 1 1 0 0 1-.347.352.978.978 0 0 1-.513.137H14v-2l-4 3 4 3v-2h4.601c.278 0 .552-.037.811-.109a2.948 2.948 0 0 0 1.319-.776c.178-.179.332-.38.456-.593a2.992 2.992 0 0 0 .336-2.215 3.163 3.163 0 0 0-.299-.764zM5.862 11.039l-2.31 4.62a3.06 3.06 0 0 0-.261.755 2.997 2.997 0 0 0 .851 2.735c.178.174.376.326.595.453A3.022 3.022 0 0 0 6.236 20H8v-2H6.236a1.016 1.016 0 0 1-.5-.13.974.974 0 0 1-.353-.349 1 1 0 0 1-.149-.468.933.933 0 0 1 .018-.245c.018-.087.048-.173.089-.256l2.256-4.512 1.599.923L8.598 8 4 9.964l1.862 1.075zm12.736 1.925L19.196 8l-1.638.945-2.843-5.117a2.95 2.95 0 0 0-1.913-1.459 3.227 3.227 0 0 0-.772-.083 3.003 3.003 0 0 0-1.498.433A2.967 2.967 0 0 0 9.41 3.944l-.732 1.464 1.789.895.732-1.465c.045-.09.101-.171.166-.242a.933.933 0 0 1 .443-.27 1.053 1.053 0 0 1 .53-.011.963.963 0 0 1 .63.485l2.858 5.146L14 11l4.598 1.964z"></path></svg>
 "@
 $ArchiveIcon = @"
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6h-3v2H9v2h2v2H9v2h2v8H7v-6h2v-2H7V8h2V6H7V4h2V2H6zm7 2 5 5h-5V4z"></path><path d="M8 15h2v2H8z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <title>Archive VM to Zip</title>
+
+  <!-- File Shape (White) -->
+  <path d="M6 2C5.4 2 5 2.4 5 3V21C5 21.6 5.4 22 6 22H18C18.6 22 19 21.6 19 21V7L14 2H6Z" fill="white"></path>
+
+  <!-- Rotated ZIP Text (Transparent Cutout) -->
+  <text x="15" y="15" font-family="Arial, sans-serif" font-size="5" font-weight="bold" fill="orange" transform="rotate(90,16,8)">ZIP ></text>
+</svg>
+
 "@
 # End General Icons
 
@@ -512,8 +521,9 @@ $htmlVirtualMachines=@"
 	Write-host "Request for $path"
 	$pattern = "/(.*?)/"
 	$selectedUri=($path -split $pattern)[1]
-    
-	Write-Host "SelectedURI: [$selectedUri]"
+    if($selectedUri.length -gt 0) {
+		Write-Host "SelectedURI: [$selectedUri]"
+	}
 	$path=$path.TrimEnd("/")
 	$htmlResponse=$htmlHead.Replace("[SELECTED]",$selectedUri)
 	
@@ -848,7 +858,7 @@ $htmlVirtualMachines=@"
 			$htmlResponse+="<H1>Storage</H1>"
 			$htmlHomePage=""
 			# Find each volume
-			Get-Volume | where {$_.DriveLetter -notlike ''} | Sort-Object DriveLetter | % {
+			Get-Volume | where {$_.DriveLetter -notlike '' -and $_.DriveType -eq 'Fixed'} | Sort-Object DriveLetter | % {
 				$volume = $(Get-Volume -DriveLetter $_.DriveLetter)
 								
 				$FreeSpace=[int]$($volume.SizeRemaining / 1GB)
@@ -859,8 +869,8 @@ $htmlVirtualMachines=@"
 				Write-Debug "$FreePercent ($FreePercent -gt 20)"
 				switch ([int]$FreePercent) {
 					{$_ -lt 10} { $Status="critical" }
-					{$_ -in 10..20} { $Status="warning" }
-					{$_ -gt 20} { $Status="good" }
+					{$_ -in 10..30} { $Status="warning" }
+					{$_ -gt 30} { $Status="good" }
 				}
 				Write-host "$($volume.DriveLetter) Drive Status $Status"
 				$FreePercent=$FreePercent.toString("#.#")
